d09a4e74f2346db8faadf17c7db0bb7b955db1ff
bender <kudin.dima@gmail.com>
2026-04-06 17:44:22 +0000

    Update src/app/portfolio/page.tsx

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -42,7 +42,7 @@ export default function PortfolioPage() {
                         textboxLayout="default"
                         useInvertedBackground={false}
                         features={[
-                            { id: "1", title: "Feature Project", description: "A deep dive into our signature aesthetic.", tag: "Highlights", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3BDuqwqQlRlHsoRKyycolI5jIQr/uploaded-1775495523742-28vmxhdo.jpg" },
+                            { id: "1", title: "Feature Project", description: "A deep dive into our signature aesthetic.", tag: "Highlights", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3BDuqwqQlRlHsoRKyycolI5jIQr/uploaded-1775497448470-kzejnmoa.jpg" },
                             { id: "2", title: "Wedding Memories", description: "Capturing love with a retro touch.", tag: "Events", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3BDuqwqQlRlHsoRKyycolI5jIQr/uploaded-1775495523742-q1iiyvah.jpg" },
                             { id: "3", title: "Studio Portraits", description: "Classic studio vibes meets film texture.", tag: "Portraits", imageSrc: "http://img.b2bpic.net/free-photo/medium-shot-woman-with-albinism-posing-studio_23-2150535752.jpg?_wi=2" },
                         ]}
